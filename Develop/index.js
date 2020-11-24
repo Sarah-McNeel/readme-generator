582d7371996dd1326501db53fe69621610746d10
SDM80 <sdawn8912@gmail.com>
2020-11-24 12:59:02 -0600

    complete with error

--- Develop/index.js
+++ Develop/index.js
@@ -1,18 +1,92 @@
-const inquire = require(inquirer);
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./src/generateMarkdown.js');
 
 // array of questions for user
-const questions = [
+const questions = () => {
+    return inquirer.prompt([
+        // Title of ReadMe
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the Title of your README? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter the title')
+                    return false
+                }
+            }
+        },
+        // Description of Project
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Description of your project. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description');
+                    return false;
+                }
+            }
+        },
+        // INSTALL
+        {
+            type: 'text',
+            name: 'install',
+            message: 'Type in some installation intructions: (Max-5)'
+        },
+        // USAGE
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter usage instructions: '
+        },
+        // Questions for Github info
+        {
+            type: 'text',
+            name: 'github',
+            message: 'What is your github username?'
+        },
+        // Questions for Email
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your current email?'
+        },
 
-];
+        // License info
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'What is the license for this project?',
+            choices: ['MIT', 'Apache', 'GPL']
+        }
+    ]);
+};
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Success! Your file had been generated')
+
+    })
 }
-
-// function to initialize program
-function init() {
-
-}
-
 // function call to initialize program
+function init() {
+    questions()
+        .then(questionsData => {
+            return generateMarkdown('questionsData');
+        })
+        .then(writeMarkDown => {
+            return writeFile(writeMarkdown);
+        })
+};
+
 init();
--- Develop/src/generateMarkdown.js
+++ Develop/src/generateMarkdown.js
@@ -1,8 +1,34 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Table of Contents:
+  - [License](#license)
+  - [Description](#description)
+  - [Images](#images)
+  - [Installation Instructions](#installation-Instructions)
+  - [Test](#test)
+  - [GitHub](#gitHub)
+  - [Email](#email-address)
+  - [Contributors](#contributors)
+  ## License:
+  [![License:${data.license}](https://img.shields.io/badge/License-${data.license}-yellow.svg)](https://opensource.org/licenses/${data.license})
+  ## Description:
+  ${data.description}
+  ## Images:
+  ![Project ScreenShot](./imagePathHere.png)
+  ![Project Screenshot](./imagePathHere.png)
+  ![Project Screenshot](./imagePathHere.png)
+  ## Installation Instructions: 
+  ${data.installationInstructions}
+  ## Test: 
+  To test type ${data.test} into the terminal
+  ## Github: 
+  Check out more project on my Github at https://github.com/${data.gitHub}
+  ## Email Address:
+  If you have any question please feel free to email me at ${data.email}
+  ## Contributors:
+  ${data.contributors}
+  `;
+};
 
-`;
-}
-
-module.exports = generateMarkdown;
+module.exports = 'generateMarkdown';
